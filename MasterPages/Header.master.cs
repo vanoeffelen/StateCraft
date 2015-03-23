59630e5ef664992080f9eaf122fd0d1127bb4e41
vanoeffelen <jared.vanoeffelen@kentinnovatonhigh.org>
2015-03-23 11:09:13 -0400

    Revert "h"
    This reverts commit 6ece196ed6aedb61b4f28ec5fbc4ea1f73140e41.

--- MasterPages/Header.master.cs
+++ MasterPages/Header.master.cs
@@ -11,8 +11,12 @@ public partial class MasterPages_Header : System.Web.UI.MasterPage
     {
 
     }
-<<<<< HEAD
+<<<<<<< HEAD
     protected void lnkbtnHome_Click(object sender, EventArgs e)
 =======
     protected void lnkbtnNews_Click(object sender, EventArgs e)
->>>>>>> originmaster
+>>>>>>> origin/master
+    {
+
+    }
+}
